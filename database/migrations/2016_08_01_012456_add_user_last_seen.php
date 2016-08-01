490a13dc9df77de57878fefbfabc1d0bcf0f118a
US VPS <root@li592-78.members.linode.com>
2016-08-01 01:40:24 +0000

    Add User Last Seen Time

---
+++ database/migrations/2016_08_01_012456_add_user_last_seen.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserLastSeen extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+		Schema::table('users', function ($table) {
+			$table->dateTime('lastSeen')->nullable();
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
